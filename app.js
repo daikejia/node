692ada08e4d472930a7f6999b12489e11246ee02
Kejia <daikejiajp@yahoo.co.jp>
2020-06-09 19:53:35 +0900

    variable port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Welcome to app");
 });
-
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
   console.log("wazapp");
 });
